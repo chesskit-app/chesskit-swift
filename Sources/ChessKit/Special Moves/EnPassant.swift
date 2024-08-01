6a8a99eeab1befba47fae95c0f2455fa24c1871d
Giuseppe <g.casillox2@gmail.com>
2024-08-01 11:20:06 +0200

    Now `couldBeCaptured(...)` function is again Internal

--- Sources/ChessKit/Special Moves/EnPassant.swift
+++ Sources/ChessKit/Special Moves/EnPassant.swift
@@ -23,7 +23,7 @@ struct EnPassant: Equatable, Hashable, Sendable {
     /// same rank as the target pawn and exactly 1 file away from the
     /// target pawn for this method to return `true`, otherwise `false`
     /// is returned.
-    public func couldBeCaptured(by capturingPiece: Piece) -> Bool {
+    func couldBeCaptured(by capturingPiece: Piece) -> Bool {
         capturingPiece.kind == .pawn &&
         capturingPiece.color == pawn.color.opposite &&
         capturingPiece.square.rank == pawn.square.rank &&
